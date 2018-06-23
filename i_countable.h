adb8afec1b3a16d0e51b0e35c21a857414d7e380
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-06-23 15:37:05 +0200

    Initial commit. First versions of basic interfaces.

---
+++ i_countable.h
@@ -0,0 +1,10 @@
+#ifndef INTERFACES_I_COUNTABLE_H
+#define INTERFACES_I_COUNTABLE_H
+
+class i_countable
+{
+  public:
+    virtual long getCount() = 0;
+};
+
+#endif //INTERFACES_I_COUNTABLE_H
---
+++ i_weightable.h
@@ -0,0 +1,13 @@
+#ifndef INTERFACES_I_WEIGHTABLE_H
+#define INTERFACES_I_WEIGHTABLE_H
+
+class i_weightable
+{
+  public:
+    virtual double getWeight() = 0;
+    virtual void setWeight(double newWeight) = 0;
+  protected:
+    double weight = 0.0;
+};
+
+#endif //INTERFACES_I_WEIGHTABLE_H
