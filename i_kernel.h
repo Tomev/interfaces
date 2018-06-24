e2eb9b2e82e5b213278847590031e0ad3a5b7d15
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-06-24 19:41:20 +0200

    Kernels getU and getW methods has been made static.

--- i_kernel.h
+++ i_kernel.h
@@ -8,18 +8,18 @@
 class i_kernel : public i_singleArgumentFunction
 {
   public:
-    virtual double getW() = 0;
-    virtual double getU() = 0;
+    virtual static double getW() = 0;
+    virtual static double getU() = 0;
 };
 
 typedef std::shared_ptr<i_kernel> kernelPtr;
 
 enum kernelIDs
 {
-  NORMAL = 0,
-  EPANECZNIKOW = 1,
-  TRIANGLE = 2,
-  DULL = 3
+  NORMAL        = 0,
+  EPANECZNIKOW  = 1,
+  TRIANGLE      = 2,
+  DULL          = 3
 };
 
 #endif //KERNELDENSITYESTIMATOR_I_KERNEL_H
