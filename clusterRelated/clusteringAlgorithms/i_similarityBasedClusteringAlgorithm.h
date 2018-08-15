a6666641ba460c3609f2c977aa931589ab54adf8
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-08-15 22:00:37 +0200

    Similarity based clustering algorithm briefly commented.

--- clusterRelated/clusteringAlgorithms/i_similarityBasedClusteringAlgorithm.h
+++ clusterRelated/clusteringAlgorithms/i_similarityBasedClusteringAlgorithm.h
@@ -4,6 +4,9 @@
 #include "i_clusteringAlgorithm.h"
 #include "../i_clusterSimilarityMeasure.h"
 
+/**
+ *  Interface for similarity based clustering algorithms (such as AHC).
+ */
 class i_similarityBasedClusteringAlgorithm : public i_clusteringAlgorithm {
   protected:
     std::shared_ptr<i_clusterSimilarityMeasure> clusterSimilarityMeasure;
